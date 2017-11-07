303659203893d8238eaf00924cc6884335674485
cliff538 <cliff538@yahoo.com>
2017-11-07 13:01:39 -0700

    added background color change slider.

--- UISlider01/ViewController.swift
+++ UISlider01/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	// MARK: - View Did Load
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		//loads initial view with "Hello" alpha at current slider position. Using slider: UISlider! outlet.
+		helloLabel.textColor = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: CGFloat(slider.value))
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	// MARK: - Outlets
+	@IBOutlet weak var helloLabel: UILabel!
+	@IBOutlet weak var slider: UISlider!
+	@IBOutlet var backgroundView: UIView!
+	@IBOutlet weak var backgroundSlider: UISlider!
+	
+	
+	
+	
+	
+	// MARK: - Interactions
+	@IBAction func dragSlider(_ sender: UISlider) {
+		//testing: print("\(sender.value)")
+		
+		//using sender.value from slider to alter the alpha:
+		helloLabel.textColor = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: CGFloat(sender.value))
+		}
+	
+	@IBAction func backgroundSlider(_ sender: UISlider) {
+		//change color of background: rainbow colors.
+		backgroundView.backgroundColor = UIColor(hue: CGFloat(sender.value), saturation: CGFloat(sender.value), brightness: CGFloat(sender.value), alpha: CGFloat(sender.value))
 	}
-
+	
+	
+	
 
 }
 
